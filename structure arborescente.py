9b7268343c23846aa03b57e76e6788610023d165
mducos <103144484+mducos@users.noreply.github.com>
2022-05-21 20:45:33 +0200

    Update structure arborescente.py

--- structure arborescente.py
+++ structure arborescente.py
@@ -12,15 +12,16 @@ def vocab2tree(file):
     vers des dictionnaires.
     Lorsqu'un mot se termine, la valeur est un dictionnaire vide.
     Schéma :
-    {"a": {"r": {"b": {"r": {"e": {}
-                             "i": {"s": {"s": {"a": {"u": {"t": {}}}}}}}}}
-           "v": {"o": {"i": {"r": {}}}}}, 
+    {"a": {"r": {"b": {"r": {"e": {"*": {}}
+                             "i": {"s": {"s": {"a": {"u": {"t": {"*": {}}}}}}}}}}
+           "v": {"o": {"i": {"r": {"*": {}}}}}}, 
      "b": {...}
     }
     '''
 
     # lecture du fichier 
     lines = open(file, "r", encoding="utf-8").readlines()
+
     # création de la structure arborescente
     voca2tree = dict()
 
@@ -29,18 +30,17 @@ def vocab2tree(file):
 
         # chaque colonne étant séparée par une tabulation, on les récupère dans une liste
         line = line.split("\t")
-        #print("line :", line)# TODO
-        # simplification des variables
-        word = line[0]
-        #print("word :", word)# TODO
-        # création des clés correspondant à la première lettre de chaque mot
-        
+
+        # simplification des variables auxquelles on ajoute un * pour signifier une fin de mot (caractère unique comme <f>)
+        word = line[0] + "*"
+
+        # création des clés correspondant à la première lettre de chaque mot, par récurrence
         letter2letter_maker(word, voca2tree)
     
-    # sérialization à l'aide de json 
-    with open("C:/Users/carol/Desktop/L3/Projet_TAL/structure_arborescente.ttl", "w", encoding='utf-8' ) as df:
+    # sérialization à l'aide de json (ie enregistrement dans un fichier pour pouvoir l'utiliser plus tard dans d'autres programmes)
+    with open("C:/Users/carol/Desktop/L3/Projet_TAL/structure_arborescente.ttl", "w", encoding='utf-8' ) as df: # chemin d'accès pour Mathilde
+    #with open("", "w", encoding='utf-8' ) as df: # chemin d'accès pour Xin
         json.dump(voca2tree, df, ensure_ascii=False)
-    #print(voca2tree)        
 
 
 
@@ -48,33 +48,24 @@ def letter2letter_maker(word, letter2letter):
     '''
     Fonction définie par récursion qui, à partir d'un mot, construit 
     la suite de dictionnaire qui y correspond.
-    "avoir" → {'a': {'v': {'o': {'i': {'r': {}}}}}}
+    "avoir*" → {'a': {'v': {'o': {'i': {'r': {'*': {}}}}}}}
     '''
     # cas de base, si le mot est fini on retourne un dictionnaire vide
     if len(word) == 1:
+        # si le mot n'existait pas déjà, on crée la feuille en la rendant terminal
         if not(word[0] in letter2letter):
-            #print(word[0], "not in dico", letter2letter)
-            tmp = dict()
-            tmp["*"] = dict()
-            #letter2letter[word[0]] = tmp
-            #print(type(word[0]))
             letter2letter[word[0]] = dict()
-            
+        # si le mot existait déjà dans le dictionnaire sur un noeud mais qu'il ne terminait pas, on le rend terminal
         else:
-            tmp = dict()
-            tmp[word] = "*"
-            #letter2letter = {**letter2letter[word[0]], **tmp}
             letter2letter = {**letter2letter[word[0]], **dict()}
         return letter2letter
     # si le mot n'est pas fini, on construit le dictionnaire qui correspond au reste du mot
     else:
+        # si le noeud de la lettre n'existe pas, on la crée
         if not(word[0] in letter2letter):
-            #print(word[0], "not in dico")
             letter2letter[word[0]] = dict()
-            #print("dico :", letter2letter)
-        #letter2letter[word[0]] = {**letter2letter, }
+        # on entre dans le sous-arbre relié par le noeud et on continue la lecture du mot
         letter2letter[word[0]] = {**letter2letter[word[0]], **letter2letter_maker(word[1:], letter2letter[word[0]])}
-        #print("dico", letter2letter, "\n")
         return letter2letter
 
 
